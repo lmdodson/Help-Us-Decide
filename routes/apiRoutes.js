7b86e15d676fe2887d871c184f564335b48c4503
Leah Dodson <leahdodson9@gmail.com>
2020-03-29 01:14:22 -0500

    configured the database call for movie info and the api call for movie titles

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,29 +1,44 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all examples
-  app.get("/api/movies", function(req, res) {
+  app.get("/api/movies", function (req, res) {
     db.movies
       .findAll({
         where: {
           category: "Adventure"
         },
-        limit: 10
+        limit: 5
       })
-      .then(function(test) {
+      .then(function (test) {
         res.json(test);
         // console.log(test);
       });
   });
 
-  app.get("/api/users", function(req, res) {
+  // get all movies of a specific genre
+  // Get route for returning posts of a specific category
+  app.get("/api/movies/category/:genre", function (req, res) {
+    db.movies
+      .findAll({
+        where: {
+          category: req.params.genre
+        },
+        limit: 5
+      })
+      .then(function (dbMovies) {
+        res.json(dbMovies);
+      });
+  });
+
+  app.get("/api/users", function (req, res) {
     db.users
       .findAll({
         // where: {
         //   user: req.params.user,
         // }
       })
-      .then(function(test) {
+      .then(function (test) {
         res.json(test);
       });
   });
